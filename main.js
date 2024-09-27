ee6c754d51d340cfe264b675415828ae2763d8dc
Iker Galarraga Insausti <157799330+IkerGI28@users.noreply.github.com>
2024-09-27 17:09:05 +0000

    ia amaituta, margin batzuekin arazoak

--- main.js
+++ main.js
@@ -32,4 +32,43 @@ const datubasea = [
     }
   ]
 
-  let argazkiak = ["https://covers.openlibrary.org/b/id/"+datubasea[0].filename, "https://covers.openlibrary.org/b/id/"+datubasea[1].filename, "https://covers.openlibrary.org/b/id/"+datubasea[2], "https://covers.openlibrary.org/b/id/"+datubasea[3],]
+  let argazkiak = ["https://covers.openlibrary.org/b/id/"+datubasea[0].filename, "https://covers.openlibrary.org/b/id/"+datubasea[1].filename, "https://covers.openlibrary.org/b/id/"+datubasea[2].filename, "https://covers.openlibrary.org/b/id/"+datubasea[3].filename]
+  
+  let unekoindex = 0;
+
+  let argazkia = document.getElementById("liburuaren-irudia");
+  let izenburua = document.getElementById("izenburua")
+  let egilea = document.getElementById("egilea")
+  let isbn = document.getElementById("isbn")
+  let data = document.getElementById("data")
+  let aurrekoa_botoia = document.getElementById("aurrekoa")
+  let hurrengoa_botoia = document.getElementById("hurrengoa")
+
+  let argazkiaeguneratu = () => {
+    argazkia.src = argazkiak[unekoindex];
+    izenburua.value = datubasea[unekoindex].izenburua;
+    egilea.value = datubasea[unekoindex].egilea;
+    isbn.value = datubasea[unekoindex].isbn;
+    data.value = datubasea[unekoindex].data;
+  }
+
+  argazkiaeguneratu();
+
+  let hurrengoargazkiaerakutsi = (event) => {
+    event.preventDefault();
+    if(unekoindex < argazkiak.length - 1){
+      unekoindex = unekoindex + 1;
+      argazkiaeguneratu();
+    }
+  }
+
+  let aurrekoargazkiaerakutsi = (event) => {
+    event.preventDefault();
+    if(unekoindex > 0){
+      unekoindex = unekoindex - 1;
+      argazkiaeguneratu();
+    }
+  }
+
+aurrekoa_botoia.onclick = aurrekoargazkiaerakutsi;
+hurrengoa_botoia.onclick = hurrengoargazkiaerakutsi;
